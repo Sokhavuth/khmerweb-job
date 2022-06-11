c5ec660814b2fa5da2e6dbcff8c2ca6e0b1ac3df
Sokhavuth <vuthdevelop@gmail.com>
2022-06-11 10:18:46 +0700

    Make it better

--- route/login.js
+++ route/login.js
@@ -3,8 +3,12 @@ import express from 'express'
 const loginRoute = express.Router()
 
 loginRoute.get('/login',async (req,res,next)=>{
-    const module = await import('../controller/login/getLogin.js')
-    module.default(req,res)
+    if(req.session.user){
+        res.redirect('/admin/job')
+    }else{
+        const module = await import('../controller/login/getLogin.js')
+        module.default(req,res)
+    }
 })
 
 loginRoute.post('/login',async (req,res,next)=>{
